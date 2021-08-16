21a6b57b11e1bd329eb69b796ecd656f458eda76
Amit Alon <amit@kaholo.io>
2021-08-16 12:39:05 +0300

    small fix

--- src/app.js
+++ src/app.js
@@ -1,21 +1,17 @@
 function alertWebhook(req, res, settings, triggerControllers) {
     try { 
-        const messages = req.body.messages;
-        messages.forEach(eventBody => {
-            const eventType = eventBody.event; // Get event type
-            const eventTitle = eventBody.incident.title; // Get event title
-            const urgency = eventBody.incident.urgency; // Get event severity 
-            if (!eventType || !eventTitle || !urgency){
-                res.status(422).send("bad pagerduty alert format");
-                throw "bad pagerduty alert format"; // throw to end loop
-            }
-            triggerControllers.forEach(trigger => {
-                const trigEventType = trigger.params.EVENT || "any";
-                const trigUrgency = trigger.params.urgency || "any";
-                if (trigEventType !== "any" && trigEventType !== eventType) return;
-                if (trigUrgency !== "any" && trigUrgency !== urgency) return;
-                trigger.execute(eventTitle, eventBody);
-            });
+        const event = req.body.event;
+        const eventType = event.event_type; // Get event type
+        const {title, urgency} = event.data;
+        if (!eventType || !title || !urgency){
+            res.status(422).send("bad pagerduty alert format");
+        }
+        triggerControllers.forEach(trigger => {
+            const trigEventType = trigger.params.EVENT || "any";
+            const trigUrgency = trigger.params.urgency || "any";
+            if (trigEventType !== "any" && trigEventType !== eventType) return;
+            if (trigUrgency !== "any" && trigUrgency !== urgency) return;
+            trigger.execute(title, event);
         });
         res.status(200).send("OK");
     }

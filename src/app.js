ca4d7d34207269ad6137081eab8a83910e4cca04
Amit Alon <amit@kaholo.io>
2021-08-23 11:27:21 +0300

    Returned trigger to v3 webhook format

--- src/app.js
+++ src/app.js
@@ -1,28 +1,31 @@
 function alertWebhook(req, res, settings, triggerControllers) {
     try { 
-        const msgs = req.body.messages;
-        const finishedTrigs = [];
-        msgs.forEach(msg => {
-            const eventType = msg.event;
-            const title = msg.incident.title;
-            const urgency = msg.incident.service.incident_urgency_rule.urgency;
-            if (!eventType || !title || !urgency){
-                throw res.status(400).send("bad pagerduty alert format");
-            }
-            triggerControllers.forEach(trigger => {
-                const trigEventType = trigger.params.EVENT || "any";
-                const trigUrgency = trigger.params.urgency || "any";
-                if (trigEventType !== "any" && trigEventType !== eventType) return addTrigger(finishedTrigs, trigger, "Event Type");
-                if (trigUrgency !== "any" && trigUrgency !== urgency) return addTrigger(finishedTrigs, trigger, "Urgency");
-                trigger.execute(`PagerDuty Incident: ${title}`, msg);
-                addTrigger(finishedTrigs, trigger);
-            });
-        });
+        const event = req.body.event;
+        const eventType = event.event_type; // Get event type
+        const {title, urgency} = event.data;
+        if (!eventType || !title || !urgency){
+            res.status(400).send("bad pagerduty alert format");
+        }
+        const msg = `Pager Duty Incident: ${title}`;
+        const finishedTrigs = filterTriggers(triggerControllers, eventType, urgency, msg, event);
         res.status(200).send(finishedTrigs);
     }
     catch (err){
         res.status(500).send(err.message);
     }
+}
+
+function filterTriggers(triggers, eventType, urgency, msg, body){
+    const finishedTrigs = [];
+    triggers.forEach(trigger => {
+        const trigEventType = trigger.params.EVENT || "any";
+        const trigUrgency = trigger.params.urgency || "any";
+        if (trigEventType !== "any" && trigEventType !== eventType) return addTrigger(finishedTrigs, trigger, "Event Type");
+        if (trigUrgency !== "any" && trigUrgency !== urgency) return addTrigger(finishedTrigs, trigger, "Urgency");
+        trigger.execute(msg, body);
+        addTrigger(finishedTrigs, trigger);
+    });
+    return finishedTrigs;
 }
 
 function addTrigger(arr, trigger, error){

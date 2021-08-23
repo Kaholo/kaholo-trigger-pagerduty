61104eba8ee91b8b39df74fe9fcd19984986ce4f
Amit Alon <amit@kaholo.io>
2021-08-23 10:19:36 +0300

    small fix

--- src/app.js
+++ src/app.js
@@ -1,23 +1,33 @@
 function alertWebhook(req, res, settings, triggerControllers) {
     try { 
-        const event = req.body.event;
-        const eventType = event.event_type; // Get event type
-        const {title, urgency} = event.data;
-        if (!eventType || !title || !urgency){
-            res.status(422).send("bad pagerduty alert format");
-        }
-        triggerControllers.forEach(trigger => {
-            const trigEventType = trigger.params.EVENT || "any";
-            const trigUrgency = trigger.params.urgency || "any";
-            if (trigEventType !== "any" && trigEventType !== eventType) return;
-            if (trigUrgency !== "any" && trigUrgency !== urgency) return;
-            trigger.execute(title, event);
+        const msgs = req.body.messages;
+        const finishedTrigs = [];
+        msgs.forEach(msg => {
+            const eventType = msg.event;
+            const title = msg.incident.title;
+            const urgency = msg.incident.service.incident_urgency_rule.urgency;
+            if (!eventType || !title || !urgency){
+                throw res.status(400).send("bad pagerduty alert format");
+            }
+            triggerControllers.forEach(trigger => {
+                const trigEventType = trigger.params.EVENT || "any";
+                const trigUrgency = trigger.params.urgency || "any";
+                if (trigEventType !== "any" && trigEventType !== eventType) return addTrigger(finishedTrigs, trigger, "Event Type");
+                if (trigUrgency !== "any" && trigUrgency !== urgency) return addTrigger(finishedTrigs, trigger, "Urgency");
+                trigger.execute(`PagerDuty Incident: ${title}`, msg);
+                addTrigger(finishedTrigs, trigger);
+            });
         });
-        res.status(200).send("OK");
+        res.status(200).send(finishedTrigs);
     }
     catch (err){
-        res.status(422).send(err.message);
+        res.status(500).send(err.message);
     }
+}
+
+function addTrigger(arr, trigger, error){
+    if (error) trigger.error = error;
+    arr.push(trigger);
 }
 
 module.exports = {
